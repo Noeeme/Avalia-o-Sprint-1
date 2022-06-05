4962f49e9f2ea5acfd2421f6849b5f5ca3d4007a
Noeeme <noemisilva598@gmail.com>
2022-06-04 21:04:30 -0300

    Adicionei desconto

--- src/Calculo.java
+++ src/Calculo.java
@@ -24,19 +24,19 @@ public class Calculo {
             System.out.println("Salário do funcionário: ");
             funcionario.salario[cont] = ler.nextFloat();
 
-                if (funcionario.salario[cont] <= 1000) {
+                if (funcionario.salario[cont] <= 1000){
                     funcionario.bonus[cont] = (float) (funcionario.salario[cont] * 0.2);
                     funcionario.salarioLiquido[cont] = funcionario.salario[cont] + funcionario.bonus[cont];
                     cont++;
                     ler.nextLine();
-                } else if (funcionario.salario[cont] > 1000 || (funcionario.salario[cont] < 2000)) {
+                } else if (funcionario.salario[cont] > 1000 && (funcionario.salario[cont] < 2000)) {
                     funcionario.bonus[cont] = (float) (funcionario.salario[cont] * 0.1);
                     funcionario.salarioLiquido[cont] = funcionario.salario[cont] + funcionario.bonus[cont];
                     cont++;
                     ler.nextLine();
                 } else {
-                    funcionario.bonus[cont] = (float) (funcionario.salario[cont] * -0.1);
-                    funcionario.salarioLiquido[cont] = funcionario.salario[cont] + funcionario.bonus[cont];
+                    funcionario.bonus[cont] = (float) (funcionario.salario[cont] * (0.1));
+                    funcionario.salarioLiquido[cont] = funcionario.salario[cont] - funcionario.bonus[cont];
                     cont++;
                     ler.nextLine();
                 }
--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,45 +1,7 @@
 public class Funcionario {
-    public void setQtdFuncionarios(int qtdFuncionarios) {
-        this.qtdFuncionarios = qtdFuncionarios;
-    }
-
-    public void setBonus(float[] bonus) {
-        this.bonus = bonus;
-    }
-
-    public void setNome(String[] nome) {
-        this.nome = nome;
-    }
-
-    public void setSalario(float[] salario) {
-        this.salario = salario;
-    }
-
-    public void setSalarioLiquido(float[] salarioLiquido) {
-        this.salarioLiquido = salarioLiquido;
-    }
-
-    public float[] getBonus() {
-        return bonus;
-    }
-
-    public float[] getSalario() {
-        return salario;
-    }
-
-    public float[] getSalarioLiquido() {
-        return salarioLiquido;
-    }
-
-    public int getQtdFuncionarios() {
-        return qtdFuncionarios;
-    }
-
-    public String[] getNome() {
-        return nome;
-    }
 
     public String nome[];
+
     public float salario[];
     public float bonus[];
     public float salarioLiquido[];
